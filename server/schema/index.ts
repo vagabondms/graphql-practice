7753dc62898364c7a91c921e22c9ab569cc7e971
vagabondms <hiseokseok@gmail.com>
2022-09-25 17:50:20 +0900

    mutation 추가

--- server/schema/index.ts
+++ server/schema/index.ts
@@ -3,13 +3,17 @@ import {
   GraphQLList,
   GraphQLSchema,
   GraphQLID,
+  GraphQLNonNull,
+  GraphQLString,
+  GraphQLEnumType,
+  GraphQLBoolean,
 } from "graphql";
 import Project from "./project";
 import { projects, clients } from "../sampleData";
 import Client from "./client";
 
 const RootQuery = new GraphQLObjectType({
-  name: "RootQueryType",
+  name: "Query",
   fields: {
     projects: {
       type: new GraphQLList(Project),
@@ -40,8 +44,60 @@ const RootQuery = new GraphQLObjectType({
   },
 });
 
+const RootMutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    createProject: {
+      type: Project,
+      args: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        description: { type: new GraphQLNonNull(GraphQLString) },
+        status: {
+          type: new GraphQLEnumType({
+            name: "status",
+            values: {
+              progress: { value: "In Progress" },
+              done: { value: "Done" },
+            },
+          }),
+        },
+        clientId: {
+          type: new GraphQLNonNull(GraphQLString),
+        },
+      },
+      resolve(_, args) {
+        const newProject = {
+          id: projects.length + 1,
+          ...args,
+        };
+
+        projects.push(newProject);
+
+        return newProject;
+      },
+    },
+    deleteProject: {
+      type: GraphQLBoolean,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLID) },
+      },
+      resolve(_, args) {
+        const targetId = projects.findIndex(
+          (project) => project.id === args.id
+        );
+        if (targetId !== undefined) {
+          projects.splice(targetId, 1);
+          return true;
+        }
+        return false;
+      },
+    },
+  },
+});
+
 const schema = new GraphQLSchema({
   query: RootQuery,
+  mutation: RootMutation,
 });
 
 export default schema;

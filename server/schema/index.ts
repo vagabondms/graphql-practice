f6bca89381d75fc0015801af259594b042aa6244
vagabondms <hiseokseok@gmail.com>
2022-09-25 17:21:37 +0900

    query 정의

--- server/schema/index.ts
+++ server/schema/index.ts
@@ -1,6 +1,12 @@
-import { GraphQLObjectType, GraphQLList, GraphQLSchema } from "graphql";
+import {
+  GraphQLObjectType,
+  GraphQLList,
+  GraphQLSchema,
+  GraphQLID,
+} from "graphql";
 import Project from "./project";
 import { projects, clients } from "../sampleData";
+import Client from "./client";
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
@@ -9,6 +15,26 @@ const RootQuery = new GraphQLObjectType({
       type: new GraphQLList(Project),
       resolve() {
         return projects;
+      },
+    },
+    clients: {
+      type: new GraphQLList(Client),
+      resolve() {
+        return clients;
+      },
+    },
+    client: {
+      type: Client,
+      args: { id: { type: GraphQLID } },
+      resolve(_, args) {
+        return clients.find((item) => item.id === args.id);
+      },
+    },
+    project: {
+      type: Project,
+      args: { id: { type: GraphQLID } },
+      resolve(_, args) {
+        return projects.find((item) => item.id === args.id);
       },
     },
   },
